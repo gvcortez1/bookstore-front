a212003d2fbac7a045e882f28b033d056bf64ddc
gvcortez1 <gvcortez1@gmail.com>
2022-07-31 21:27:46 -0300

    ajustes no component-read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HomeComponent } from './components/views/home/home.component';
 import { FooterComponent } from './components/template/footer/footer.component';
@@ -39,6 +40,7 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     , MatCardModule
     , MatTableModule
     , HttpClientModule
+    , MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
